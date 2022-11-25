0e8591d76255119649efa19bc4ba29aa4a279af3
GurpreetSingh2601 <gurubahre2019@gmail.com>
2022-11-24 20:36:40 -0800

    update

--- Processing_Service/app.py
+++ Processing_Service/app.py
@@ -62,26 +62,25 @@ DB_ENGINE = create_engine(f"sqlite:///{path}")
 #DB_ENGINE = create_engine("sqlite:///%s" %app_config["datastore"]["filename"])
 Base.metadata.bind = DB_ENGINE
 DB_SESSION = sessionmaker(bind=DB_ENGINE)
-
-def get_stats():
-    logger.info('Request has been started')
-    session = DB_SESSION()
-    results = session.query(Stats).order_by(Stats.last_updated.desc())
+session = DB_SESSION()
+results = session.query(Stats).order_by(Stats.last_updated.desc())
+session.close()
+def get_stats(results):
+    logger.info('Request has been started')     
     if not results:
         logger.error("Statistics does not exist")
         return 404
-    
     #logger.debug(f"contents of python dictionary {results[-1].to_dict()}")
     logger.info("The request has been completed")
-    session.close()
+    
     return results[0].to_dict(), 200
 
-def populate_stats():
+def populate_stats(results):
     """ Periodically update stats """
     logger.info('Period processing has been started')
-    session = DB_SESSION()
+    #session = DB_SESSION()
     current_timestamp = datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%f")
-    results = session.query(Stats).order_by(Stats.last_updated.desc())
+    #results = session.query(Stats).order_by(Stats.last_updated.desc())
     try:
         last_updated = str(results[0].last_updated)
         a,b = last_updated.split(" ")
